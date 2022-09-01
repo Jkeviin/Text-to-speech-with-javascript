6e1872a8ffd876c514eff42ea6a1330b006b7e9b
Kevin <slash2130kevin@gmail.com>
2022-09-01 16:51:21 -0500

    Comentarios agregados

--- Script.js
+++ Script.js
@@ -6,9 +6,22 @@ document.getElementById('hablar').addEventListener('click', () => {
 
 
 const decir = (texto) => {
-    speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+    speechSynthesis.speak(new SpeechSynthesisUtterance(texto), {lang: 'es-ES'}, {rate: 1.0}, {pitch: 1.0}, {volume: 1.0}, {voice: 'native'});
+    /*
+    rate: es para el velocidad de la voz
+    pitch: es para el tono de la voz
+    volume: es para el volumen de la voz
+    voice: es para la voz que se usara
+
+    Tipos de voz:
+    native: es la voz nativa del navegador
+    es-ES: es la voz española
+    en-US: es la voz inglesa
+    hombre: es la voz de un hombre
+    mujer: es la voz de una mujer
+    */
 }
-if(speechSynthesis.onvoiceschanged !== undefined){
+/* if(speechSynthesis.onvoiceschanged !== undefined){
     speechSynthesis.onvoiceschanged = function(){
         let voices = speechSynthesis.getVoices();
         voices.forEach(voice => {
@@ -21,4 +34,4 @@ if(speechSynthesis.onvoiceschanged !== undefined){
             }
         } );
     }
-}
+} */
